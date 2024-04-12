dfd9965a78303b685981ad9155dc38e2b554e274
Crissper1 <cris.per.146@gmail.com>
2024-04-12 10:36:54 -0300

    commit PCHome Actualizacion

--- Programacion2024/SegundoPractico/prac2dos.java
+++ Programacion2024/SegundoPractico/prac2dos.java
@@ -2,21 +2,31 @@ package SegundoPractico;
 
 public class prac2dos {
 
-    static void dosnum(int largo, int ancho) {
-        
-        for (int i = 0; i < ancho; i++) {
-            for (int j = 0; j < largo; j++) {
-                if (i == 0 || i == ancho - 1 || j == 0 || j == largo - 1) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
+    static void dosnum(int largo , int ancho){
+
+        for (int i = 0; i <= ancho; i++) {
+            
+            for (int j = 0; j <= largo; j++) {
+                if (i>=1 && i <= ancho -2){
+                    if (j<1 || j == largo ){
+                        System.out.print("*");
+                    }
+                    if (j==1|| j < largo -1 ){
+                        System.out.print(" ");
+                    }      
                 }
+                if (i==0 || i == ancho -1){
+                    System.out.print("*");
+                }
+                
             }
             System.out.println();
-        }
+    }
+        
     }
     public static void main(String[] args) {
         dosnum(7, 5);
 
     }
 }
+
