6067c46a943b0b0bb2e22ab8a1ed01c771f5c3af
Crissper1 <cris.per.146@gmail.com>
2024-04-30 10:44:46 -0300

    Actualizacion

--- Programacion2024/SegundoPractico/ejer3ro.java
+++ Programacion2024/SegundoPractico/ejer3ro.java
@@ -7,16 +7,22 @@ import java.util.Scanner;
 // ▪ Validar que las unidades sean válidas
 // ▪ Validar que las unidades no sean iguales
 public class ejer3ro {
+    static void dato(){
+      
+    }
 
-    public static String[] num_and_Medida() {
-        Scanner input = new Scanner(System.in);
-        String[] temN = new String[2]; // Cambié la capacidad a 2 para almacenar el número y la medida
-        System.out.print("Ingrese N° a convertir: ");
-        temN[0] = input.nextLine();
-        System.out.print("Ingrese la primera letra de su medida de temperatura (Celsius, Fahrenheit, Kelvin): ");
-        temN[1] = input.nextLine();
-        input.close();
-        return temN;
+    public static int ValorN() {
+      Scanner dato = new Scanner(System.in);
+      int valor;
+      System.out.print("Ingrese valor a convertir: ");
+      do {
+        if(!dato.hasNextInt()){
+          System.out.print("Dato erroneo!!!\nVuelva a Ingresar: ");
+          dato.next();
+        }
+      }while(!dato.hasNextInt());
+      valor=dato.nextInt();
+      return valor;
     }
     
     public static void main(String[] args) {
