45a497ecd16e55e2eb6db63ebc764790bd68af4c
Crissper1 <cris.per.146@gmail.com>
2024-05-14 17:04:22 -0300

    cambios 1.0

--- Programacion2024/SegundoPractico/eje3ro_2.java
+++ Programacion2024/SegundoPractico/eje3ro_2.java
@@ -18,7 +18,7 @@ public class eje3ro_2 {
         return valor;
     }
     static char medInicial(){
-        char m = Character.toUpperCase(dato.next().charAt(0));
+        char m = Character.toUpperCase(dato.nextLine().charAt(0));
         boolean bandera = false;
         do {
           if(m=='C'|| m=='F'||m=='K'){
@@ -28,23 +28,45 @@ public class eje3ro_2 {
             m = Character.toUpperCase(dato.next().charAt(0));
           }
           }while(!bandera);
-          
           return m;
         }
-    static String cambMedida(){
-        
+    static char cambMedida(){
         char letra = medInicial(), cM=' ';
         
         if (letra=='C'){
+            do {
             System.out.print("Puede convertir\nCelsius a Fahrenheit 'F'\n"+
                                               "Celsius a Kelvin 'K': ");
+            cM=Character.toUpperCase(dato.next().charAt(0));
+            if(cM!='F'|| cM!='K'){
+                System.out.println("Dato Incorrecto!!!Vuleva a Intenta");
+            }
+            }while(cM!='F'|| cM!='K');
         }
-   
-    } 
-    
+        if (letra=='F'){
+            do {
+            System.out.print("Puede convertir\nFahrenheit a Celsius 'C'\n"+
+                                              "Fahrenheit a Kelvin 'K': ");
+            cM=Character.toUpperCase(dato.next().charAt(0));
+            if(cM!='F'|| cM!='K'){
+                System.out.println("Dato Incorrecto!!!Vuleva a Intenta");
+            }
+            }while(cM!='C'|| cM!='K');
+        }
+        if (letra=='K'){
+            do {
+            System.out.print("Puede convertir\nKelvin a Celsius 'C'\n"+
+                                              "Kelvin a Fahrenheit 'F': ");
+            cM=Character.toUpperCase(dato.next().charAt(0));
+            if(cM!='F'|| cM!='K'){
+                System.out.println("Dato Incorrecto!!!Vuleva a Intenta");
+            }
+            }while(cM!='F'|| cM!='C');
+        }
+        return cM;
+    }
     static double recibe(int n, String medActual, String med_a_conver){
-        
-        
+
         return 0;
     }
     public static void main(String[] args) {
@@ -56,7 +78,8 @@ public class eje3ro_2 {
         System.out.print("Ingrese su medidada de temperatura\n" +
                          "Celsius la letra 'C'\nFahrenheit la letra 'F'\n" +
                          "Kelvin la letra 'K' :");
-        System.out.println(valorM+" "+medInicial());
+        char mI=medInicial(), cM=cambMedida();
+        System.out.println(valorM+" "+mI+"Valor a convertir"+ cM);
 
         dato.close();
     }
